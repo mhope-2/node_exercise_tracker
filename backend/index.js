534334c1576b01ddb5dd829ecf611966c7d3f6c1
mhope <hopemichael720@gmail.com>
2026-08-06 10:21:42 +0200

    chore: fix dependabot issues

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,38 @@
 const User = require('./models/users.model')
 const Exercise = require("./models/exercise.model");
 
-const AdminBro = require('admin-bro')
-const AdminBroExpress = require('@admin-bro/express')
+const AdminJS = require('adminjs')
+const AdminJSExpress = require('@adminjs/express')
 
 const express = require('express')
 const app = express()
 
-///////
-const AdminBroMongoose = require('@admin-bro/mongoose')
+const AdminJSMongoose = require('@adminjs/mongoose')
 const mongoose = require('mongoose')
-AdminBro.registerAdapter(AdminBroMongoose)
+AdminJS.registerAdapter({ Resource: AdminJSMongoose.Resource, Database: AdminJSMongoose.Database })
 
 
 const run = async () => {
-  const connection = await mongoose.connect('mongodb+srv://mhope:mhope@cluster1.330al.mongodb.net/cluster1_db?retryWrites=true&w=majority').then(()=>{
+  await mongoose.connect('mongodb+srv://mhope:mhope@cluster1.330al.mongodb.net/cluster1_db?retryWrites=true&w=majority').then(()=>{
     console.log(`MongoDB database connection established Successfully`);
     }).catch((error)=>{
         console.log("MongoDB not connected");
         console.log(error);
     });
 
-  const adminBro = new AdminBro ({
-    Databases: [connection],
+  const admin = new AdminJS({
+    databases: [mongoose.connection],
     resources: [User, Exercise],
     rootPath: '/admin',
-})
+  })
 
-    const router = AdminBroExpress.buildRouter (adminBro)
-    //////////////////
-    app.use(adminBro.options.rootPath, router)
-
+  await admin.initialize()
+  const router = AdminJSExpress.buildRouter(admin)
+  app.use(admin.options.rootPath, router)
 }
 run()
 
-app.listen(8080, () => console.log('AdminBro is running at localhost:8080/admin'))
+app.listen(8080, () => console.log('AdminJS is running at localhost:8080/admin'))
 
 
 
